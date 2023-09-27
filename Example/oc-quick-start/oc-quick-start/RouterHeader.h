43726c0cf05c992379d63ac24e54998af7b23ee3
coooliang <wot_chenliang_xdl@cibdev.com>
2023-09-27 10:02:38 +0800

    add to sourcetree

--- Example/oc-quick-start/oc-quick-start/RouterHeader.h
+++ Example/oc-quick-start/oc-quick-start/RouterHeader.h
@@ -7,10 +7,5 @@
 #ifndef RouterHeader_h
 #define RouterHeader_h
 
-#define NextStep @"NextStep"
-#define Home @"App://home"
-#define Fortune @"App://fortune"
-#define Wallet @"App://wallet"
-#define Account @"App://account"
 
 #endif /* RouterHeader_h */
