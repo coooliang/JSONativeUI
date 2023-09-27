c17dbab64cd2eddba43145fe1dbcfab8c2e2cca8
coooliang <wot_chenliang_xdl@cibdev.com>
2023-09-27 10:05:30 +0800

    add UIView+JSONativeUI

---
+++ JSONativeUI/Classes/UIView+JSONativeUI.h
@@ -0,0 +1,15 @@
+//// 
+//  UIView+JSONativeUI.h
+//  Pods-oc-quick-start
+//  Created by ___ORGANIZATIONNAME___ on 2023/9/27
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (JSONativeUI)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JSONativeUI/Classes/UIView+JSONativeUI.m
@@ -0,0 +1,11 @@
+//// 
+//  UIView+JSONativeUI.m
+//  Pods-oc-quick-start
+//  Created by ___ORGANIZATIONNAME___ on 2023/9/27
+//
+
+#import "UIView+JSONativeUI.h"
+
+@implementation UIView (JSONativeUI)
+
+@end
